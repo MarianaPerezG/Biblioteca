0f6af2ef012cb504c181c916889450fdd528f7e0
Mariana Perez <marianaperez@Marianas-MacBook-Pro.local>
2018-05-14 10:39:53 +0200

    Test Bibkioteca Sys

--- test/com/twu/biblioteca/BibliotecaSystemTest.java
+++ test/com/twu/biblioteca/BibliotecaSystemTest.java
@@ -12,6 +12,7 @@ public class BibliotecaSystemTest {
     public Biblioteca biblioteca;
     public BibliotecaSystem  bibliotecaSys;
     public Book book;
+    public Movie movie;
     public Printer printer;
     public InputReader inputreader;
 
@@ -22,20 +23,63 @@ public class BibliotecaSystemTest {
 
         biblioteca = new Biblioteca("Test");
         book = new Book("Test", "Test Author", 2018);
+        movie = new Movie("Test Movie", "Test director", 2018, 5.0f);
         biblioteca.getBookList().add(book);
         bibliotecaSys = new BibliotecaSystem(biblioteca, printer);
+        biblioteca.getMovieList().add(movie);
 
     }
 
     @Test
-    public void testIfAnAvailableBookCanBeCheckedOut(){
+    public void shouldCheckOutAnAvailableBook(){
         assertEquals(bibliotecaSys.checkOut(book), true);
     }
 
     @Test
-    public void testIfAnUnavailableCantBeCheckedOut(){
+    public void shouldNotCheckOutIfAnUnavailableBook(){
         book.checkOut();
         assertEquals(bibliotecaSys.checkOut(book), false);
+
+    }
+
+    @Test
+    public void shouldCheckOutIfAnAvailableMovie(){
+        assertEquals(bibliotecaSys.checkOut(movie), true);
+    }
+
+    @Test
+    public void shouldNotCheckOutIfAnUnavailableMovie(){
+        movie.checkOut();
+        assertEquals(bibliotecaSys.checkOut(movie), false);
+    }
+
+    @Test
+    public void shouldNotAllowCheckOutIfNotMatchingBook(){
+
+        assertEquals(bibliotecaSys.manageResponseToCheckoutBook("Random"), false);
+
+    }
+
+    @Test
+    public void shouldAllowCheckOutIfNotMatchingBook(){
+
+        assertEquals(bibliotecaSys.manageResponseToCheckoutBook("Test"), true);
+        assertEquals(book.isAvailable(), false);
+
+    }
+
+    @Test
+    public void shouldNotAllowCheckOutIfNotMatchingMovie(){
+
+        assertEquals(bibliotecaSys.manageResponseToCheckoutMovie("Random"), false);
+
+    }
+
+    @Test
+    public void shouldAllowCheckOutIfNotMatchingMovie(){
+
+        assertEquals(bibliotecaSys.manageResponseToCheckoutMovie("Test"), true);
+        assertEquals(movie.isAvailable(), false);
 
     }
 
